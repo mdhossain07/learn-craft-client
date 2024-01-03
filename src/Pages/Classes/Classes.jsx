9767918c12777b9db153ac848cc4bfa881834f49
mdhossain07 <mrabir8097@gmail.com>
2024-01-03 15:46:17 +0600

    data filtering implemented

--- src/Pages/Classes/Classes.jsx
+++ src/Pages/Classes/Classes.jsx
@@ -5,15 +5,13 @@ import Container from "../../Components/Shared/Container";
 import Cover from "../../Components/Shared/Cover/Cover";
 import teacher from "../../assets/images/Teacher-pana.png";
 import { PuffLoader } from "react-spinners";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const Classes = () => {
   const axiosPublic = useAxiosPublic();
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const [sortOptions, setSortOptions] = useState("");
-  const [sortedResults, setSortedResults] = useState([]);
-  const [allData, setAllData] = useState(false);
+  const [sortOptions, setSortOptions] = useState("All Courses");
 
   const { data: approvedClass, isLoading } = useQuery({
     queryKey: ["approved-class"],
@@ -32,16 +30,30 @@ const Classes = () => {
       .then((res) => setSearchResults(res.data));
   };
 
-  axiosPublic.get(`/api/v1/courses/sort?sort=${sortOptions}`).then((res) => {
-    setSortedResults(res.data);
-    setAllData(true);
-  });
+  const asecSort = useCallback(() => {
+    const result = approvedClass?.sort((a, b) => a.price - b.price);
+    return result;
+  }, [approvedClass]);
+
+  const descSort = useCallback(() => {
+    const result = approvedClass?.sort((a, b) => b.price - a.price);
+    return result;
+  }, [approvedClass]);
+
+  // axiosPublic.get(`/api/v1/courses/sort?sort=${sortOptions}`).then((res) => {
+  //   console.log(res.data);
+  //   setSortedResults(res.data);
+  // });
 
   useEffect(() => {
-    if (approvedClass) {
+    if (sortOptions === "All Courses") {
       setSearchResults(approvedClass);
+    } else if (sortOptions === "High To Low") {
+      setSearchResults(asecSort);
+    } else if (sortOptions === "Low To High") {
+      setSearchResults(descSort);
     }
-  }, [approvedClass]);
+  }, [approvedClass, asecSort, descSort, sortOptions]);
 
   return (
     <>
@@ -77,43 +89,39 @@ const Classes = () => {
         {isLoading ? (
           <PuffLoader color="#36d7b7" />
         ) : (
-          <div>
-            {sortOptions && (
-              <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 justify-items-center">
-                {sortedResults?.map((item) => (
-                  <div key={item?._id}>
-                    <div className="h-[480px] shadow-xl p-5 rounded-lg space-y-2">
-                      <img
-                        className="w-[800px] h-[200px]"
-                        src={item.image}
-                        alt={item.title}
-                      />
-                      <h2 className="text-2xl font-semibold">{item.title}</h2>
-                      <p>{item?.description.slice(0, 30)}....</p>
-                      <div className="flex gap-10">
-                        <p className="text-gray-800 text-sm font-medium">
-                          Course Price: ${item.price}
-                        </p>
-                        <p className="text-gray-800 text-sm font-medium">
-                          Total Enrollment:{" "}
-                          {item?.enrollment ? item?.enrollment : "0"}
-                        </p>
-                      </div>
-
-                      <p className="text-gray-800 text-sm font-medium">
-                        Instructor Name: {item.instructor_name}
-                      </p>
-
-                      <Link to={`/class-info/${item._id}`}>
-                        <button className="btn bg-[#0766AD] w-full mt-4 text-white p-3 rounded-lg">
-                          Enroll Now
-                        </button>
-                      </Link>
-                    </div>
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 justify-items-center">
+            {searchResults?.map((item) => (
+              <div key={item?._id}>
+                <div className="h-[480px] shadow-xl p-5 rounded-lg space-y-2">
+                  <img
+                    className="w-[800px] h-[200px]"
+                    src={item.image}
+                    alt={item.title}
+                  />
+                  <h2 className="text-2xl font-semibold">{item.title}</h2>
+                  <p>{item?.description.slice(0, 30)}....</p>
+                  <div className="flex gap-10">
+                    <p className="text-gray-800 text-sm font-medium">
+                      Course Price: ${item.price}
+                    </p>
+                    <p className="text-gray-800 text-sm font-medium">
+                      Total Enrollment:{" "}
+                      {item?.enrollment ? item?.enrollment : "0"}
+                    </p>
                   </div>
-                ))}
+
+                  <p className="text-gray-800 text-sm font-medium">
+                    Instructor Name: {item.instructor_name}
+                  </p>
+
+                  <Link to={`/class-info/${item._id}`}>
+                    <button className="btn bg-[#0766AD] w-full mt-4 text-white p-3 rounded-lg">
+                      Enroll Now
+                    </button>
+                  </Link>
+                </div>
               </div>
-            )}
+            ))}
           </div>
         )}
       </Container>
